cf344a14725a7a2392f4ad57b6532b7c993ece1b
rugayyaabdullayeva <rugayya.abdullayeva@gmail.com>
2023-10-12 10:22:19 +0400

    file created

---
+++ bf103-js/app.js
@@ -0,0 +1,45 @@
+// TASK 1
+// let num = 25;
+// let lastNum = num % 10;
+// let secondNum = parseInt(num / 10) % 10
+// let firstNum = parseInt(num / 100)
+
+// if (lastNum == secondNum || ( secondNum == firstNum) || (lastNum == firstNum)) {
+//     console.log(true)
+// }
+// else {
+//     console.log(false)
+// }
+
+
+// TASK 2
+// let a = 9
+// let b = 8
+// let c = 2
+
+// if ((a < b) && (a < c)) {
+//     console.log(a);
+// } else if ((b > a) && (b > c)) {
+//     console.log(b);
+// } else if ((c > a) && (c > b)) {
+//     console.log(c);
+// }
+// else {
+//     console.log(invalid)
+// }
+
+
+// TASK 3
+let a = 2;
+let b = 6;
+
+if ((a > 5) && (b > 5)) {
+    console.log(a + b);
+}
+else {
+    console.log(difference)
+}
+
+
+
+// TASK 4 
